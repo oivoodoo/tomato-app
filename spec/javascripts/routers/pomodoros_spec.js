780ab09e353a27ff14f8624def201e619844b952
Alexandr Korsak <alex.korsak@gmail.com>
2012-02-08 23:21:39 +0300

    Add specs for testing backbone behaviour in the app

---
+++ spec/javascripts/routers/pomodoros_spec.js
@@ -0,0 +1,8 @@
+describe('Pomodoros Router', function() {
+  beforeEach(function() {
+    this.controller = App.Routers.Pomodoros();
+  });
+
+  it("should use list of pomodoros previously saved to the local storage", function() {
+  });
+});
