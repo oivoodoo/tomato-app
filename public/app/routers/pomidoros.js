acbb0cb4f289639f4edc3d42d9aacfbd26248450
Alexandr Korsak <alex.korsak@gmail.com>
2012-02-08 20:18:30 +0300

    Add default router for serving the root path

---
+++ public/app/routers/pomidoros.js
@@ -0,0 +1,10 @@
+var App.Routers.Pomidoros = Backbone.Router.extend({
+  routes: {
+    '/', 'index'
+  }
+
+  index: function() {
+    // Catch up all pomodoros from localStorage and then show in the listview.
+    //
+  }
+});
