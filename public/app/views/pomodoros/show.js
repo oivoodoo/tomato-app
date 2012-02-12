f15d9a92f50463a44178203e3cd31abcc0f0fc46
Alexandr Korsak <alex.korsak@gmail.com>
2012-02-13 00:58:27 +0300

    Add default timer for the show page

--- public/app/views/pomodoros/show.js
+++ public/app/views/pomodoros/show.js
@@ -8,7 +8,31 @@ App.Views.Pomodoros.Show = Backbone.View.extend({
     _.bindAll(this);
     $('#show .remove').bind('click', this.remove);
     $('#show .cancel').bind('click', this.cancel);
-    $('#show [data-role=footer] li').bind('click', this.start);
+
+    var self = this;
+    $('#show [data-role=footer] li').bind('click', function() {
+      $.mobile.changePage('#timer', 'pop', true, true);
+
+      var minutes = $(this).find('a').text();
+      var seconds = 0;
+
+      self.timer = setInterval(function() {
+        if (seconds % 60 === 0) {
+          minutes -= 1;
+          seconds = 60;
+        }
+        seconds -= 1;
+
+        if (minutes === 0) {
+          seconds = 0;
+          clearInterval(timer);
+          self.timer = null;
+        }
+
+        $('#timer .minutes').html(minutes);
+        $('#timer .seconds').html(seconds);
+      }, 1000);
+    });
     $('#timer .cancel').bind('click', function(event) {
       event.preventDefault();
       $('#timer').dialog('close');
@@ -33,15 +57,18 @@ App.Views.Pomodoros.Show = Backbone.View.extend({
     this.goHome();
   },
 
-  start: function() {
-    $.mobile.changePage('#timer', 'pop', true, true);
-  },
-
   leave: function() {
     $('#show .cancel').unbind('click');
     $('#show .remove').unbind('click');
     $('#show [data-role=footer] li').unbind('click');
     $('#timer .click').unbind('click');
+    // timer preferences
+    $('#timer .minutes').text(0);
+    $('#timer .seconds').text(0);
+
+    if (this.timer) {
+      clearInterval(this.timer);
+    }
   },
 
   goHome: function() {
