0a47d7a82095f2a3891bc74c6332d330d8c6d7d0
Alexandr Korsak <alex.korsak@gmail.com>
2012-09-07 18:48:43 +0300

    Add manifest for the market and icons

--- server.rb
+++ server.rb
@@ -7,3 +7,9 @@ get '/' do
   send_file File.join(settings.public_folder, 'index.html')
 end
 
+get '/manifest' do
+  content_type 'application/x-web-app-manifest+json'
+
+  send_file File.join(settings.public_folder, 'manifest.txt')
+end
+
